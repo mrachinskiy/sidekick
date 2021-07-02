91453756f7f7f50f07d0566e6282b1f8718987a8
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2021-07-02 23:15:45 +0400

    Refactor: os > pathlib

--- var.py
+++ var.py
@@ -19,7 +19,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 
-import os
+from pathlib import Path
 
 from . import report
 
@@ -32,5 +32,5 @@ Report = report.Data()
 
 
 ADDON_ID = __package__
-ADDON_DIR = os.path.dirname(__file__)
-CONFIG_DIR = os.path.join(ADDON_DIR, ".config")
+ADDON_DIR = Path(__file__).parent
+CONFIG_DIR = ADDON_DIR / ".config"
