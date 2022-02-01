f9ad84d5cde45438313f539fc350f4169dab545d
Mikhail Rachinskiy <mikhail.rachinskiy@gmail.com>
2022-02-01 20:38:30 +0400

    Copyright year update

--- problemlib.py
+++ problemlib.py
@@ -1,7 +1,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  Sidekick scene linter for Blender.
-#  Copyright (C) 2020  Mikhail Rachinskiy
+#  Copyright (C) 2020-2022  Mikhail Rachinskiy
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
